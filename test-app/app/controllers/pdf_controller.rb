835b447d125d787a7828bee9843b98a3d7c53405
Matt Moretti <werebus@gmail.com>
2018-06-05 12:12:20 -0400

    Mostly a version bump, but also:
    * Got rid of a 404 for /favicon.ico
    * Removed application_controller
    * Tried to prune Rails bits a bit more
    
    That said, one can't actually remove the unused parts of Rails because
    prawn_rails has all of rails as a dependency. Oh well.

--- test-app/app/controllers/pdf_controller.rb
+++ test-app/app/controllers/pdf_controller.rb
@@ -1,4 +1,6 @@
-class PdfController < ApplicationController
+class PdfController < ActionController::Base
+  protect_from_forgery with: :exception
+
   def static
     respond_to do |format|
       format.pdf { render pdf: 'static' }
--- test-app/config/application.rb
+++ test-app/config/application.rb
@@ -1,7 +1,5 @@
 require_relative 'boot'
 
-require "rails"
-require "active_model/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
 Bundler.require(*Rails.groups)
