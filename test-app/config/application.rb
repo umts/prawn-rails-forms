d66f6f4b264b1ab3b05ba1d619199b513c4093a5
Matt Moretti <werebus@gmail.com>
2019-08-27 11:22:26 -0400

    Bumped gems
    Including upgrading the test app to Rails 5.2

--- test-app/config/application.rb
+++ test-app/config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 
 module TestApp
   class Application < Rails::Application
-    config.load_defaults 5.1
+    config.load_defaults 5.2
     config.generators.system_tests = nil
   end
 end
